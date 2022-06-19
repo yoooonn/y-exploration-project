495f670a864b6307c8fa55d7241e1bdb9285141c
yooonn <ycourlee@qq.com>
2022-06-19 13:39:41 +0800

    = refactor shells.

---
+++ utils/install-base-dao.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#author yooonn
+
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+BLUE=$(tput setaf 4)
+RESET=$(tput sgr0)
+
+# functions
+printCurrentBranch() {
+    echo "${GREEN}current branch: ${BLUE}$1${RESET}"
+}
+
+# contents
+currentBranchName=$(git symbolic-ref --short HEAD)
+
+printCurrentBranch "${currentBranchName}"
+
+./mvnw clean install -pl base/base-dao -am -Pbase-include
+
+exit 0
