8cd3ecb5c7a3990680e7c16b3bd2e05e2f6364c5
yooonn <ycourlee@qq.com>
2022-11-12 16:11:19 +0800

    feat: add nacos experimental module

---
+++ ex-notes/spring-cloud/nacos/config/src/main/java/com/ycourlee/explore/notes/cloud/NacosConfigApplication.java
@@ -0,0 +1,16 @@
+package com.ycourlee.explore.notes.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yooonn
+ * @date 2022.11.12
+ */
+@SpringBootApplication
+public class NacosConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigApplication.class, args);
+    }
+}
